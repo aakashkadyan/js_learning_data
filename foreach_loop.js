f31dde12133c6914209650395a22446f9f7633f5
Aakash1910 <aakash.kadiyan@strategycoglobal.com>
2024-07-31 22:45:26 +0530

    foreach loop program practice!

---
+++ foreach_loop.js
@@ -0,0 +1,13 @@
+// array.forEach((element, index, array) => {
+// code to execute for each element
+// });
+
+
+let fruits = ['apple','banana','date','fig']
+
+fruits.forEach((fruit, index) => {
+
+    console.log(`${index + 1}: ${fruit}`);
+
+
+});
