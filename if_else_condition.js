911031478aa3b4d64cb774b833f008c1d9637fb6
Aakash1910 <aakash.kadiyan@strategycoglobal.com>
2024-09-18 11:43:05 +0530

    updated conditional statements

--- if_else_condition.js
+++ if_else_condition.js
@@ -1,7 +1,31 @@
-let year = prompt('How old you are?', '');
+// let year = prompt('How old you are?', '');
 
-if (year == 25) {
-  alert( 'You guessed it right!' );
-} else {
-  alert( 'How can you be so wrong?' ); // any value except 2015
+// if (year == 25) {
+//   alert( 'You guessed it right!' );
+// } else {
+//   alert( 'How can you be so wrong?' ); // any value except 2015
+// }
+
+
+let age = 17;
+
+if (age>18){
+  console.log("You can vote!!!");
+}
+else{
+  console.log("You can't Vote!!");
+}
+
+
+
+let num = 17;
+
+if (num%2 === 0){
+
+  console.log("Number is even!!!");
+
+}
+else {
+
+  console.log("Number is odd!!")
 }
