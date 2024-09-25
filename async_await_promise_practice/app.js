7d8c49be80210da30d6c2e0da2280b473252824a
Aakash1910 <aakash.kadiyan@strategycoglobal.com>
2024-09-25 22:21:37 +0530

    learnt and practiced about promises & Async await

--- async_await_promise_practice/app.js
+++ async_await_promise_practice/app.js
@@ -1,46 +1,46 @@
-// console.log("one");
-// console.log("two");
-// console.log("three");
+console.log("one");
+console.log("two");
+console.log("three");
 
-// function hello (){
+function hello (){
 
-//     console.log("hello");
+    console.log("hello");
 
-// }
+}
 
-// setTimeout(hello,5000);
-
-
-//callback
-
-// function sum(a,b) {
-
-//     console.log(a+b);
-
-// }
-
-// function addition (a, b, sumCallback) {
+setTimeout(hello,5000);
 
 
-//     sumCallback(a,b);
-// }
+callback
 
-// addition(1,2, sum)
+function sum(a,b) {
+
+    console.log(a+b);
+
+}
+
+function addition (a, b, sumCallback) {
 
 
+    sumCallback(a,b);
+}
 
-
-// const hello_2 = () =>{
-
-//     console.log("hello i am here!!");
-
-// }
-
-// setTimeout(hello_2, 4000)
+addition(1,2, sum)
 
 
 
-//callback hell----
+
+const hello_2 = () =>{
+
+    console.log("hello i am here!!");
+
+}
+
+setTimeout(hello_2, 4000)
+
+
+
+callback hell----
 function getData(dataId, getNewData) {
 
     setTimeout (() =>{
@@ -66,10 +66,43 @@ getData(1, ()=>{
 
 //promises...
 
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am in Promise!!");
+        resolve("success!");
+        //reject("error....");
+});
+};
 
-let promise = new Promise((resolve, reject) =>{
-    console.log("I am in Promise!!");
-    resolve("success!");
+let promise = getPromise();
+
+promise.then((res) =>{                            // we use then method once it is resolved...
+    console.log("fullfilled  : ", res);
 });
 
-//then and catch...
+promise.catch((err) =>{                          // we use catch method once it is rejected...
+    console.log("Error occured not fullfilled : ", err)
+});
+
+
+
+//aync await 
+
+
+function getAllData(dataId) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            console.log("got data", dataId);
+            resolve("got data sucessfully");
+        }, 2000);
+    });
+}
+
+async function data(){
+    await getAllData(1);
+    await getAllData(2);
+    await getAllData(3);
+}
+
+data();
