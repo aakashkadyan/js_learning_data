b3123e722b1a757d8498516bc9a21d6512fdd35e
Aakash1910 <aakash.kadiyan@strategycoglobal.com>
2024-09-30 09:39:09 +0530

    learnt and practiced about non-premitive object like key value pair

--- objects.js
+++ objects.js
@@ -10,6 +10,14 @@ let employee ={
 };
 console.log(employee['name']);
 
-employee.name = 'ravi';
+employee.name = 'ravi'; // update name with ravi
 console.log(employee['name']);
+console.log(employee.salary); 
+employee.salary = 12000; // updated salary
 console.log(employee.salary);
+
+
+for (const key in employee){ // for in loop
+    console.log(key); // only keys
+    console.log(`${key}: ${employee[key]}`); // print key with values using for in loop
+}
